356c5032d07b56673613948032dededd5d11ee68
maartenbreddels <maartenbreddels@gmail.com>
2017-01-10 11:12:59 +0100

    added custom transfer function

--- ipyvolume/__init__.py
+++ ipyvolume/__init__.py
@@ -1,7 +1,8 @@
 from ._version import version_info, __version__
 
 from .volume import *
-
+from .transferfunction import *
+from .examples import  *
 def _jupyter_nbextension_paths():
     return [{
         'section': 'notebook',
