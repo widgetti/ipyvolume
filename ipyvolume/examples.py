84c34e96de5cd267e72f9a21adc401577856b8ac
maartenbreddels <maartenbreddels@gmail.com>
2017-01-20 21:49:01 +0100

    generalized a bit

--- ipyvolume/examples.py
+++ ipyvolume/examples.py
@@ -2,15 +2,34 @@ import numpy as np
 import scipy.special
 import ipyvolume
 
-x, y, z = np.ogrid[-3:3:6./128,-3:3:6./128,-3:3:6./128]
-r = np.linalg.norm([x,y,z])
-
-theta = np.arctan2(y, x)
-phi = np.arccos(z/r)
-
 __all__ = ["example_ylm"]
 
-def example_ylm(**kwargs):
+def xyz(shape=128, limits=[-3, 3], spherical=False):
+	dim = 3
+	try:
+		shape[0]
+	except:
+		shape = [shape] * dim
+	try:
+		limits[0][0]
+	except:
+		limits = [limits] * dim
+	v = [slice(vmin, vmax+(vmax-vmin)/float(N)/2, (vmax-vmin)/float(N-1)) for (vmin, vmax), N in zip(limits, shape)]
+	x, y, z = np.ogrid.__getitem__(v)
+	if spherical:
+		r = np.linalg.norm([x, y, z])
+		theta = np.arctan2(y, x)
+		phi = np.arccos(z / r)
+		return x, y, z, r, theta, phi
+	else:
+		return x, y, z
+
+
+def example_ylm(shape=128, limits=[-3, 3], **kwargs):
+	__, __, __, r, theta, phi = xyz(shape=shape, limits=limits, spherical=True)
 	radial = np.exp(-(r - 2) ** 2)
-	data = np.abs(scipy.special.sph_harm(0, 2, theta, phi) ** 2).T * radial
-	return ipyvolume.volume(data=data, **kwargs)
+	data = np.abs(scipy.special.sph_harm(0, 2, theta, phi) ** 2) * radial
+	return ipyvolume.volume(data=data.T, **kwargs)
+
+
+# http://graphics.stanford.edu/data/voldata/
