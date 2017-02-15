0d93e5f13e7a51a618c33c763054d21902e3f085
maartenbreddels <maartenbreddels@gmail.com>
2017-02-15 20:04:17 +0100

    initial py.test testing

---
+++ ipyvolume/test.py
@@ -0,0 +1,39 @@
+import ipyvolume.pylab as p3
+import ipyvolume.examples
+import ipyvolume.datasets
+import numpy as np
+import os
+if not os.path.exists("tmp"):
+    os.makedirs("tmp")
+
+f1 = p3.figure()
+f2 = p3.figure(2)
+f3 = p3.figure()
+f4 = p3.figure(2)
+
+assert f1 != f2
+assert f2 != f3
+assert f3 != f4
+assert f2 == f2
+
+x, y, z = np.random.random((3, 100))
+p3.scatter(x, y, z)
+p3.save("tmp/ipyolume_scatter.html")
+
+x, y, z, u, v, w = np.random.random((6, 100))
+p3.quiver(x, y, z, u, v, w)
+p3.save("tmp/ipyolume_quiver.html")
+
+
+x, y, z = ipyvolume.examples.xyz()
+p3.volshow(x*y*z)
+p3.save("tmp/ipyolume_volume.html")
+
+
+# just cover and call
+ipyvolume.examples.ball()
+ipyvolume.examples.example_ylm()
+
+ipyvolume.datasets.aquariusA2.fetch()
+ipyvolume.datasets.hdz2000.fetch()
+ipyvolume.datasets.zeldovich.fetch()
