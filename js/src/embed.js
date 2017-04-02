c36e123f8caac7293e21900d3b52a38099039e51
maartenbreddels <maartenbreddels@gmail.com>
2017-04-02 13:22:57 +0200

    js fix: embed.js now similar to index.js

--- js/src/embed.js
+++ js/src/embed.js
@@ -5,5 +5,7 @@
 // already be loaded by the notebook otherwise.
 
 // Export widget models and views, and the npm package version number.
-module.exports = require('./volume.js');
+var _ = require('underscore')
+// Export widget models and views, and the npm package version number.
+module.exports = _.extend({}, require('./figure.js'), require('./tf.js'), require('./scatter.js'));
 module.exports['version'] = require('../package.json').version;
