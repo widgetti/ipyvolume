f576fb6b090ef6d760604cf727615de08c7604af
maartenbreddels <maartenbreddels@gmail.com>
2017-07-08 22:54:49 +0200

    version: bump

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -1,9 +1,10 @@
-version_info = (0, 4, 0, 'alpha', 1)
+version_info    = (0, 4, 0, 'alpha', 2)
+version_info_js = (0, 4, 0, 'alpha', 1)
 #__version__ = '.'.join(map(str, version_info))
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 __version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
   '' if version_info[3]=='final' else _specifier_[version_info[3]]+str(version_info[4]))
 
-__version_js__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
-  '' if version_info[3]=='final' else '-%s.%s' % (version_info[3], str(version_info[4])))
+__version_js__ = '%s.%s.%s%s'%(version_info_js[0], version_info_js[1], version_info_js[2],
+  '' if version_info[3]=='final' else '-%s.%s' % (version_info_js[3], str(version_info_js[4])))
