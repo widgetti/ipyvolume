8218bf1535b2de3e5ce43baa5fa1f11819507dcf
stonebig <stonebig34@gmail.com>
2017-08-02 21:19:33 +0200

    works on python-3.6 (#43)
    update setup.py to reflect that it works on py3.6

--- setup.py
+++ setup.py
@@ -170,6 +170,7 @@ setup_args = {
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 }
 
