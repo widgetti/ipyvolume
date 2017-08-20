6ec26ad7ca2fa1ee2d4feeca420268aea62a355f
maartenbreddels <maartenbreddels@gmail.com>
2017-08-20 21:08:15 +0200

    use the threejs version number for embedding

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -8,3 +8,5 @@ __version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
 
 __version_js__ = '%s.%s.%s%s'%(version_info_js[0], version_info_js[1], version_info_js[2],
   '' if version_info[3]=='final' else '-%s.%s' % (version_info_js[3], str(version_info_js[4])))
+
+__version_threejs__ = '0.85' # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
