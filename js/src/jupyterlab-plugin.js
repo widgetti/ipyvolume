ad2adc872d20e3390294e971b315db90a0e6bab2
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-11-06 01:18:00 +0100

    Update to jupyterlab manager 0.29

--- js/src/jupyterlab-plugin.js
+++ js/src/jupyterlab-plugin.js
@@ -1,9 +1,9 @@
-var jupyter_volume = require('ipyvolume');
-var jupyterlab_widgets = require('@jupyter-widgets/jupyterlab-manager');
+var jupyter_volume = require('./index');
+var base = require('@jupyter-widgets/base');
 
 module.exports = {
   id: 'jupyter.extensions.jupyter-volume',
-  requires: [jupyterlab_widgets.INBWidgetExtension],
+  requires: [base.IJupyterWidgetRegistry],
   activate: function(app, widgets) {
       widgets.registerWidget({
           name: 'ipyvolume',
