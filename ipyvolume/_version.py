ed31724a4c4a8b6afcb7f294dedf5ff9967021b6
maartenbreddels <maartenbreddels@gmail.com>
2017-11-30 20:33:06 +0100

    Release 0.4.1

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -6,11 +6,11 @@ _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 __version__ = '0.4.1'
   '' if version_info[3]=='final' else _specifier_[version_info[3]]+str(version_info[4]))
 
-__version_js__ = '%s.%s.%s%s'%(version_info_js[0], version_info_js[1], version_info_js[2],
+__version_js__ = '0.4.1'
   '' if version_info[3]=='final' else '-%s.%s' % (version_info_js[3], str(version_info_js[4])))
 
 __version_tuple__ = (0, 4, 1)
 __version__ = '0.4.1'
-__version_tuple_js__ = (0, 4, 0)
-__version_js__ = '0.4.0'
+__version_tuple_js__ = (0, 4, 1)
+__version_js__ = '0.4.1'
 __version_threejs__ = '0.85' # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
