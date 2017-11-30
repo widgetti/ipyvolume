d5a620dcb35bea8e0f917b2256657e5c4ff2af6a
maartenbreddels <maartenbreddels@gmail.com>
2017-11-30 20:34:01 +0100

    version: folloing releash versioning names

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -1,14 +1,4 @@
 
-#__version__ = '.'.join(map(str, version_info))
-version_info = [0,0,0,'final',0]
-version_info_js = [0,0,0,'final',0]
-_specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
-__version__ = '0.4.1'
-  '' if version_info[3]=='final' else _specifier_[version_info[3]]+str(version_info[4]))
-
-__version_js__ = '0.4.1'
-  '' if version_info[3]=='final' else '-%s.%s' % (version_info_js[3], str(version_info_js[4])))
-
 __version_tuple__ = (0, 4, 1)
 __version__ = '0.4.1'
 __version_tuple_js__ = (0, 4, 1)
