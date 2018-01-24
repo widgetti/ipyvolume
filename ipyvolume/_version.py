877cba46caa7ece932ad50b7efad31b1e62df158
Maarten Breddels <maartenbreddels@gmail.com>
2018-01-24 14:02:05 +0100

    Release 0.4.5

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -1,6 +1,6 @@
 
-__version_tuple__ = (0, 4, 4)
-__version__ = '0.4.4'
+__version_tuple__ = (0, 4, 5)
+__version__ = '0.4.5'
 __version_tuple_js__ = (0, 4, 4)
 __version_js__ = '0.4.4'
 __version_threejs__ = '0.85' # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
