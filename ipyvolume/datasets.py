e1337673d77465132a33ddd8788c3a73d77d5f0a
Maarten Breddels <maartenbreddels@gmail.com>
2018-04-11 09:31:39 +0200

    new: astro module for astronomy related helper routines

--- ipyvolume/datasets.py
+++ ipyvolume/datasets.py
@@ -16,6 +16,38 @@ except ImportError:
 osname = dict(darwin="osx", linux="linux", windows="windows")[platform.system().lower()]
 
 
+
+class UrlCached(object):
+	def __init__(self, url):
+		self.url = url
+		self.path = os.path.join(data_dir, os.path.split(url)[1])
+
+	def download(self, force=False):
+		if not os.path.exists(self.path) or force:
+			print("Downloading %s to %s" % (self.url, self.path))
+			code = os.system(self.download_command_wget())
+			if not os.path.exists(self.path):
+				print("Download failed, exit code was: " +str(code) +" will try with curl")
+				code = os.system(self.download_command_curl())
+				if not os.path.exists(self.path):
+					print("Download failed again, exit code was: " +str(code) +" using urlretrieve")
+					self.download_urlretrieve()
+
+	def fetch(self):
+		self.download()
+		if os.path.exists(self.path):
+			return self.path
+		else:
+			raise Exception("file not found and/or download failed")
+
+	def download_command_wget(self):
+		return "wget --progress=bar:force -c -P %s %s" % (data_dir, self.url)
+
+	def download_command_curl(self):
+		return "cd %s; curl -O -L %s" % (data_dir, self.url)
+
+	def download_urlretrieve(self):
+		urlretrieve(self.url, self.path)
 class Dataset(object):
 	def __init__(self, name, density=True, numpy=False):
 		self.name = name
