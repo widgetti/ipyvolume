7617c643b5c0e513b75cbe57ef18a74e129bf1c1
Maarten Breddels <maartenbreddels@gmail.com>
2018-06-06 13:32:08 +0200

    fix: move to threejs 0.91

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -3,4 +3,4 @@ __version_tuple__ = (0, 4, 5)
 __version__ = '0.4.5'
 __version_tuple_js__ = (0, 4, 5)
 __version_js__ = '0.4.5'
-__version_threejs__ = '0.85' # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
+__version_threejs__ = '0.91' # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
