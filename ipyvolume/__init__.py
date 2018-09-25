87e8925457ccc6dd5d52d5709ea62da9f9fdb54f
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2018-09-25 16:39:57 +1000

    PEP8 imports, changed wildcard imports to explicit imports in ipyvolume __init__.py

--- ipyvolume/__init__.py
+++ ipyvolume/__init__.py
@@ -1,13 +1,57 @@
 from __future__ import absolute_import
-from ._version import __version__
 
+from ._version import __version__
 from . import styles
-from .widgets import *
-from .transferfunction import *
 from . import examples
 from . import datasets
 from . import embed
-from .pylab import *
+from .widgets import (Mesh,
+                      Scatter,
+                      Volume,
+                      Figure,
+                      quickquiver,
+                      quickscatter,
+                      quickvolshow)
+from .transferfunction import (TransferFunction,
+                               TransferFunctionJsBumps,
+                               TransferFunctionWidgetJs3,
+                               TransferFunctionWidget3)
+from .pylab import (current,
+                    clear,
+                    controls_light,
+                    figure,
+                    gcf,
+                    xlim,
+                    ylim,
+                    zlim,
+                    xyzlim,
+                    squarelim,
+                    plot_trisurf,
+                    plot_surface,
+                    plot_wireframe,
+                    plot_mesh,
+                    plot,
+                    scatter,
+                    quiver,
+                    show,
+                    animate_glyphs,
+                    animation_control,
+                    gcc,
+                    transfer_function,
+                    plot_isosurface,
+                    volshow,
+                    save,
+                    movie,
+                    screenshot,
+                    savefig,
+                    xlabel,
+                    ylabel,
+                    zlabel,
+                    xyzlabel,
+                    view,
+                    style,
+                    plot_plane',
+                    selector_default)
 
 def _jupyter_nbextension_paths():
     return [{
