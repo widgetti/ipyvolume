3293a495708d89457db270da3f62a51aa3ee7cdf
Guillaume Gay <guillaume@damcb.com>
2020-04-16 14:33:43 +0200

    update setup.py
    reflect that python 2 is not supported anymore

--- setup.py
+++ setup.py
@@ -180,13 +180,10 @@ setup_args = {
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Multimedia :: Graphics',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 }
 
