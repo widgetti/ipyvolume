0dd248896bde3f5e8c2fd6234ba7b9903f707b96
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-04-22 10:49:31 +0200

    Release 0.6.0-alpha.3 of js

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -1,7 +1,7 @@
 
 __version_tuple__ = (0, 6, 0, 'alpha.3')
 __version__ = '0.6.0-alpha.3'
-__version_tuple_js__ = (0, 6, 0, 'alpha.2')
-__version_js__ = '0.6.0-alpha.2'
+__version_tuple_js__ = (0, 6, 0, 'alpha.3')
+__version_js__ = '0.6.0-alpha.3'
 # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
 __version_threejs__ = '0.97'
