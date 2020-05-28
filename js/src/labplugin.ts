da1c1f0dc088fbcbca1bfebd23b39fb8e600ef39
Angus Hollands <goosey15@gmail.com>
2020-05-28 21:06:01 +0100

    This commit contains changes which should fix ipyvolume for JupyterLab 2.1.0 (#323)
    In utils.ts, the version string was set to a valid version, and taken from the package.json. I believe this is the true fix.
    
    labplugin.ts was modified to use the semver from utils.ts instead of index.ts (reason given below). I also added the type signatures to try and catch any typing errors, but I realise that you probably omitted these deliberately (to reduce the lib size when exporting?).
    
    index.ts was modified to remove any non-view/model/exporter exports, so it may be directly used as the `exports` argument to `registerWidget`. I'm not sure if this solves anything - but I could forsee a case where `registerWidget` expects every member of `exports` to be a view/model/exporter class.
    
    I added a version 2 `||` constraint to `@jupyter-widgets/controls`, because the latest version is 2. Not sure if this is correct.

--- js/src/labplugin.ts
+++ js/src/labplugin.ts
@@ -1,13 +1,15 @@
 import * as base from "@jupyter-widgets/base";
 import * as jupyter_volume from "./index";
+import {semver_range} from "./utils";
+import {JupyterFrontEnd, JupyterFrontEndPlugin} from '@jupyterlab/application';
 
-const plugin = {
+const plugin: JupyterFrontEndPlugin<void> = {
   id: "ipyvolume",
   requires: [base.IJupyterWidgetRegistry],
-  activate(app, widgets) {
+  activate(app: JupyterFrontEnd, widgets: base.IJupyterWidgetRegistry) {
       widgets.registerWidget({
           name: "ipyvolume",
-          version: jupyter_volume.version,
+          version: semver_range,
           exports: jupyter_volume,
       });
   },
