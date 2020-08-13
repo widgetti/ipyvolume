860a6c5c7a4a691f152539f6e31d03e6ca8a6510
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-08-13 21:06:08 +0200

    errors copy pasting shaders

--- js/glsl/mesh-fragment.glsl
+++ js/glsl/mesh-fragment.glsl
@@ -1,5 +1,15 @@
 // mesh-fragment shader
 #if defined( AS_DEFAULT ) || defined( AS_COORDINATE )
+varying vec4 vertex_color;
+varying vec3 vertex_position;
+varying vec2 vertex_uv;
+
+#ifdef USE_TEXTURE
+    uniform sampler2D texture;
+    uniform sampler2D texture_previous;
+    uniform float animation_time_texture;
+#endif
+
     // taken similar to phong shading
     #include <common>
     #include <packing>
@@ -26,75 +36,194 @@
     #include <specularmap_pars_fragment>
     #include <logdepthbuf_pars_fragment>
     #include <clipping_planes_pars_fragment>
+    void main(void)
+    {
+        #ifdef AS_COORDINATE
+            // should we ignore shading for RGB pass?
+            gl_FragColor = vec4(vertex_color.rgb, 1.0);
+        #else
+            // 0 when in shadow, 1 if visible
+            float shadow_visibility = getShadowMask();
+            #ifdef IS_LINE
+                gl_FragColor = vec4(vertex_color.rgb * shadow_visibility, vertex_color.a);
+            #else
+                vec3 fdx = dFdx( vertex_position );
+                vec3 fdy = dFdy( vertex_position );
+                vec3 normal = normalize( cross( fdx, fdy ) );
+                float diffuse = dot( normal, vec3( 0.0, 0.0, 1.0 ) );
+
+                #ifdef USE_TEXTURE
+                    vec4 sample = mix(texture2D(texture_previous, vertex_uv), texture2D(texture, vertex_uv), animation_time_texture);
+                    gl_FragColor = vec4(clamp(diffuse, 0.2, 1.) * sample.rgb * shadow_visibility, 1.0);
+                #else
+                    gl_FragColor = vec4(clamp(diffuse, 0.2, 1.) * vertex_color.rgb * shadow_visibility, vertex_color.a);
+                #endif // USE_TEXTURE
+            #endif // IS_LINE
+        #endif // AS_COORDINATE
+    }
 #endif //defined( AS_DEFAULT ) || defined( AS_COORDINATE )
+
+
+
 #ifdef AS_LAMBERT
-    #define LAMBERT
-    uniform vec3 diffuse;
-    uniform vec3 emissive;
-    uniform float emissiveIntensity;
-    uniform float opacity;
+uniform vec3 diffuse;
+uniform vec3 emissive;
+uniform float opacity;
 
-    varying vec3 vLightFront;
+varying vec3 vLightFront;
 
-    #ifdef DOUBLE_SIDED
-        varying vec3 vLightBack;
-    #endif
+#ifdef DOUBLE_SIDED
 
-    #include <common>
-    #include <packing>
-    #include <dithering_pars_fragment>
-    #include <color_pars_fragment>
-    #include <uv_pars_fragment>
-    #include <uv2_pars_fragment>
-    #include <map_pars_fragment>
-    #include <alphamap_pars_fragment>
-    #include <aomap_pars_fragment>
-    #include <lightmap_pars_fragment>
-    #include <emissivemap_pars_fragment>
-    #include <envmap_pars_fragment>
-    #include <bsdfs>
-    #include <lights_pars_begin>
-    #include <fog_pars_fragment>
-    #include <shadowmap_pars_fragment>
-    #include <shadowmask_pars_fragment>
-    #include <specularmap_pars_fragment>
-    #include <logdepthbuf_pars_fragment>
-    #include <clipping_planes_pars_fragment>
+	varying vec3 vLightBack;
+
+#endif
+
+#include <common>
+#include <packing>
+#include <dithering_pars_fragment>
+#include <color_pars_fragment>
+#include <uv_pars_fragment>
+#include <uv2_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <aomap_pars_fragment>
+#include <lightmap_pars_fragment>
+#include <emissivemap_pars_fragment>
+#include <envmap_pars_fragment>
+#include <bsdfs>
+#include <lights_pars_begin>
+#include <fog_pars_fragment>
+#include <shadowmap_pars_fragment>
+#include <shadowmask_pars_fragment>
+#include <specularmap_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <clipping_planes_pars_fragment>
+
+void main() {
+
+	#include <clipping_planes_fragment>
+
+	vec4 diffuseColor = vec4( diffuse, opacity );
+	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+	vec3 totalEmissiveRadiance = emissive;
+
+	#include <logdepthbuf_fragment>
+	#include <map_fragment>
+	#include <color_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+	#include <specularmap_fragment>
+	#include <emissivemap_fragment>
+
+	// accumulation
+	reflectedLight.indirectDiffuse = getAmbientLightIrradiance( ambientLightColor );
+
+	#include <lightmap_fragment>
+
+	reflectedLight.indirectDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb );
+
+	#ifdef DOUBLE_SIDED
+
+		reflectedLight.directDiffuse = ( gl_FrontFacing ) ? vLightFront : vLightBack;
+
+	#else
+
+		reflectedLight.directDiffuse = vLightFront;
+
+	#endif
+
+	reflectedLight.directDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb ) * getShadowMask();
+
+	// modulation
+	#include <aomap_fragment>
+
+	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + totalEmissiveRadiance;
+
+	#include <envmap_fragment>
+
+	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+
+	#include <tonemapping_fragment>
+	#include <encodings_fragment>
+	#include <fog_fragment>
+	#include <premultiplied_alpha_fragment>
+	#include <dithering_fragment>
+
+}
 #endif //AS_LAMBERT
 #ifdef AS_PHONG
-    #define PHONG
+#define PHONG
 
-    uniform vec3 diffuse;
-    uniform vec3 emissive;
-    uniform vec3 specular;
+uniform vec3 diffuse;
+uniform vec3 emissive;
+uniform vec3 specular;
+uniform float shininess;
+uniform float opacity;
 
-    uniform float shininess;
-    uniform float opacity;
-    uniform float emissiveIntensity;
+#include <common>
+#include <packing>
+#include <dithering_pars_fragment>
+#include <color_pars_fragment>
+#include <uv_pars_fragment>
+#include <uv2_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <aomap_pars_fragment>
+#include <lightmap_pars_fragment>
+#include <emissivemap_pars_fragment>
+#include <envmap_pars_fragment>
+#include <gradientmap_pars_fragment>
+#include <fog_pars_fragment>
+#include <bsdfs>
+#include <lights_pars_begin>
+#include <lights_phong_pars_fragment>
+#include <shadowmap_pars_fragment>
+#include <bumpmap_pars_fragment>
+#include <normalmap_pars_fragment>
+#include <specularmap_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <clipping_planes_pars_fragment>
 
-    #include <common>
-    #include <packing>
-    #include <dithering_pars_fragment>
-    #include <color_pars_fragment>
-    #include <uv_pars_fragment>
-    #include <uv2_pars_fragment>
-    #include <map_pars_fragment>
-    #include <alphamap_pars_fragment>
-    #include <aomap_pars_fragment>
-    #include <lightmap_pars_fragment>
-    #include <emissivemap_pars_fragment>
-    #include <envmap_pars_fragment>
-    #include <gradientmap_pars_fragment>
-    #include <fog_pars_fragment>
-    #include <bsdfs>
-    #include <lights_pars_begin>
-    #include <lights_phong_pars_fragment>
-    #include <shadowmap_pars_fragment>
-    #include <bumpmap_pars_fragment>
-    #include <normalmap_pars_fragment>
-    #include <specularmap_pars_fragment>
-    #include <logdepthbuf_pars_fragment>
-    #include <clipping_planes_pars_fragment>
+void main() {
+
+	#include <clipping_planes_fragment>
+
+	vec4 diffuseColor = vec4( diffuse, opacity );
+	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+	vec3 totalEmissiveRadiance = emissive;
+
+	#include <logdepthbuf_fragment>
+	#include <map_fragment>
+	#include <color_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+	#include <specularmap_fragment>
+	#include <normal_fragment_begin>
+	#include <normal_fragment_maps>
+	#include <emissivemap_fragment>
+
+	// accumulation
+	#include <lights_phong_fragment>
+	#include <lights_fragment_begin>
+	#include <lights_fragment_maps>
+	#include <lights_fragment_end>
+
+	// modulation
+	#include <aomap_fragment>
+
+	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
+
+	#include <envmap_fragment>
+
+	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+
+	#include <tonemapping_fragment>
+	#include <encodings_fragment>
+	#include <fog_fragment>
+	#include <premultiplied_alpha_fragment>
+	#include <dithering_fragment>
+
+}
 #endif //AS_PHONG
 #ifdef AS_PHYSICAL
     #define PHYSICAL
@@ -141,148 +270,25 @@
     #include <metalnessmap_pars_fragment>
     #include <logdepthbuf_pars_fragment>
     #include <clipping_planes_pars_fragment>
-#endif //AS_PHYSICAL
-
-varying vec4 vertex_color;
-varying vec3 vertex_position;
-varying vec2 vertex_uv;
-
-#ifdef USE_TEXTURE
-    uniform sampler2D texture;
-    uniform sampler2D texture_previous;
-    uniform float animation_time_texture;
-#endif
-
 
 void main(void)
 {
-#if defined( AS_DEFAULT ) || defined( AS_COORDINATE )
-    #ifdef AS_COORDINATE
-        // should we ignore shading for RGB pass?
-        gl_FragColor = vec4(vertex_color.rgb, 1.0);
-    #else
-        // 0 when in shadow, 1 if visible
-        float shadow_visibility = getShadowMask();
-        #ifdef IS_LINE
-            gl_FragColor = vec4(vertex_color.rgb * shadow_visibility, vertex_color.a);
-        #else
-            vec3 fdx = dFdx( vertex_position );
-            vec3 fdy = dFdy( vertex_position );
-            vec3 normal = normalize( cross( fdx, fdy ) );
-            float diffuse = dot( normal, vec3( 0.0, 0.0, 1.0 ) );
-
-            #ifdef USE_TEXTURE
-                vec4 sample = mix(texture2D(texture_previous, vertex_uv), texture2D(texture, vertex_uv), animation_time_texture);
-                gl_FragColor = vec4(clamp(diffuse, 0.2, 1.) * sample.rgb * shadow_visibility, 1.0);
-            #else
-                gl_FragColor = vec4(clamp(diffuse, 0.2, 1.) * vertex_color.rgb * shadow_visibility, vertex_color.a);
-            #endif // USE_TEXTURE
-        #endif // IS_LINE
-    #endif // AS_COORDINATE
-#endif //defined( AS_DEFAULT ) || defined( AS_COORDINATE )
-
-#ifdef AS_LAMBERT
-    #include <clipping_planes_fragment>
-
-	vec4 diffuseColor = vec4( vec3(1,1,1), opacity );
-	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-	vec3 totalEmissiveRadiance = emissive * emissiveIntensity;
-
-	#include <logdepthbuf_fragment>
-	#include <map_fragment>
-	//#include <color_fragment>
-	#include <alphamap_fragment>
-	#include <alphatest_fragment>
-	#include <specularmap_fragment>
-	#include <emissivemap_fragment>
-
-	// accumulation
-	reflectedLight.indirectDiffuse = getAmbientLightIrradiance( ambientLightColor );
-
-	#include <lightmap_fragment>
-
-	reflectedLight.indirectDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb );
-
-	#ifdef DOUBLE_SIDED
-		reflectedLight.directDiffuse = ( gl_FrontFacing ) ? vLightFront : vLightBack;
-	#else
-		reflectedLight.directDiffuse = vLightFront;
-	#endif
-
-	reflectedLight.directDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb ) * getShadowMask();
-
-	// modulation
-	#include <aomap_fragment>
-
-	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + totalEmissiveRadiance;
-
-	#include <envmap_fragment>
-
-	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
-
-	#include <tonemapping_fragment>
-	#include <encodings_fragment>
-	#include <fog_fragment>
-	#include <premultiplied_alpha_fragment>
-	#include <dithering_fragment>
-
-#endif //AS_LAMBERT
-
-#ifdef AS_PHONG
-    #include <clipping_planes_fragment>
-
-	vec4 diffuseColor = vec4( vec3(1,1,1), opacity );
-	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-	vec3 totalEmissiveRadiance = emissive * emissiveIntensity;
-
-	#include <logdepthbuf_fragment>
-	#include <map_fragment>
-	//#include <color_fragment>
-	#include <alphamap_fragment>
-	#include <alphatest_fragment>
-	#include <specularmap_fragment>
-	#include <normal_fragment_begin>
-	#include <normal_fragment_maps>
-	//normal = -normal;
-	#include <emissivemap_fragment>
-
-	// accumulation
-	#include <lights_phong_fragment>
-	#include <lights_fragment_begin>
-	#include <lights_fragment_maps>
-	#include <lights_fragment_end>
-
-	// modulation
-	#include <aomap_fragment>
-
-	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
-
-	#include <envmap_fragment>
-
-	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
-
-	#include <tonemapping_fragment>
-	#include <encodings_fragment>
-	#include <fog_fragment>
-	#include <premultiplied_alpha_fragment>
-	#include <dithering_fragment>
-#endif //AS_PHONG
-
-#ifdef AS_PHYSICAL
 	#include <clipping_planes_fragment>
 
-	vec4 diffuseColor = vec4( vec3(1,1,1), opacity );
+	vec4 diffuseColor = vec4( 1.0, 1.0, 1.0, opacity );
 	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
 	vec3 totalEmissiveRadiance = emissive * emissiveIntensity;
 
 	#include <logdepthbuf_fragment>
 	#include <map_fragment>
 	//#include <color_fragment>
+
 	#include <alphamap_fragment>
 	#include <alphatest_fragment>
 	#include <roughnessmap_fragment>
 	#include <metalnessmap_fragment>
 	#include <normal_fragment_begin>
+
 	#include <normal_fragment_maps>
 	#include <emissivemap_fragment>
 
@@ -303,5 +309,85 @@ void main(void)
 	#include <fog_fragment>
 	#include <premultiplied_alpha_fragment>
 	#include <dithering_fragment>
-#endif //AS_PHYSICAL
+
 }
+#endif //AS_PHYSICAL
+#ifdef AS_DISTANCE
+#define DISTANCE
+
+uniform vec3 referencePosition;
+uniform float nearDistance;
+uniform float farDistance;
+varying vec3 vWorldPosition;
+
+#include <common>
+#include <packing>
+#include <uv_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <clipping_planes_pars_fragment>
+
+void main () {
+
+	#include <clipping_planes_fragment>
+
+	vec4 diffuseColor = vec4( 1.0 );
+
+	#include <map_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+
+	float dist = length( vWorldPosition - referencePosition );
+	dist = ( dist - nearDistance ) / ( farDistance - nearDistance );
+	dist = saturate( dist ); // clamp to [ 0, 1 ]
+
+	gl_FragColor = packDepthToRGBA( dist );
+
+}
+#endif//AS_DISTANCE
+
+#ifdef AS_DEPTH
+#if DEPTH_PACKING == 3200
+
+	uniform float opacity;
+
+#endif
+
+#include <common>
+#include <packing>
+#include <uv_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <clipping_planes_pars_fragment>
+
+void main() {
+
+	#include <clipping_planes_fragment>
+
+	vec4 diffuseColor = vec4( 1.0 );
+
+	#if DEPTH_PACKING == 3200
+
+		diffuseColor.a = opacity;
+
+	#endif
+
+	#include <map_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+
+	#include <logdepthbuf_fragment>
+
+	#if DEPTH_PACKING == 3200
+
+		gl_FragColor = vec4( vec3( gl_FragCoord.z ), opacity );
+
+	#elif DEPTH_PACKING == 3201
+
+		gl_FragColor = packDepthToRGBA( gl_FragCoord.z );
+
+	#endif
+
+}
+#endif //AS_DEPTH
--- js/glsl/scatter-fragment.glsl
+++ js/glsl/scatter-fragment.glsl
@@ -72,73 +72,164 @@ void main(void)
 #endif //defined( AS_DEFAULT ) || defined( AS_COORDINATE )
 
 #ifdef AS_LAMBERT
-    #define LAMBERT
-    uniform vec3 diffuse;
-    uniform vec3 emissive;
-    uniform float emissiveIntensity;
-    uniform float opacity;
+uniform vec3 diffuse;
+uniform vec3 emissive;
+uniform float opacity;
 
-    varying vec3 vLightFront;
+varying vec3 vLightFront;
 
-    #ifdef DOUBLE_SIDED
-        varying vec3 vLightBack;
-    #endif
+#ifdef DOUBLE_SIDED
 
-    #include <common>
-    #include <packing>
-    #include <dithering_pars_fragment>
-    #include <color_pars_fragment>
-    #include <uv_pars_fragment>
-    #include <uv2_pars_fragment>
-    #include <map_pars_fragment>
-    #include <alphamap_pars_fragment>
-    #include <aomap_pars_fragment>
-    #include <lightmap_pars_fragment>
-    #include <emissivemap_pars_fragment>
-    #include <envmap_pars_fragment>
-    #include <bsdfs>
-    #include <lights_pars_begin>
-    #include <fog_pars_fragment>
-    #include <shadowmap_pars_fragment>
-    #include <shadowmask_pars_fragment>
-    #include <specularmap_pars_fragment>
-    #include <logdepthbuf_pars_fragment>
-    #include <clipping_planes_pars_fragment>
+	varying vec3 vLightBack;
+
+#endif
+
+#include <common>
+#include <packing>
+#include <dithering_pars_fragment>
+#include <color_pars_fragment>
+#include <uv_pars_fragment>
+#include <uv2_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <aomap_pars_fragment>
+#include <lightmap_pars_fragment>
+#include <emissivemap_pars_fragment>
+#include <envmap_pars_fragment>
+#include <bsdfs>
+#include <lights_pars_begin>
+#include <fog_pars_fragment>
+#include <shadowmap_pars_fragment>
+#include <shadowmask_pars_fragment>
+#include <specularmap_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <clipping_planes_pars_fragment>
+
+void main() {
+
+	#include <clipping_planes_fragment>
+
+	vec4 diffuseColor = vec4( diffuse, opacity );
+	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+	vec3 totalEmissiveRadiance = emissive;
+
+	#include <logdepthbuf_fragment>
+	#include <map_fragment>
+	#include <color_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+	#include <specularmap_fragment>
+	#include <emissivemap_fragment>
+
+	// accumulation
+	reflectedLight.indirectDiffuse = getAmbientLightIrradiance( ambientLightColor );
+
+	#include <lightmap_fragment>
+
+	reflectedLight.indirectDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb );
+
+	#ifdef DOUBLE_SIDED
+
+		reflectedLight.directDiffuse = ( gl_FrontFacing ) ? vLightFront : vLightBack;
+
+	#else
+
+		reflectedLight.directDiffuse = vLightFront;
+
+	#endif
+
+	reflectedLight.directDiffuse *= BRDF_Diffuse_Lambert( diffuseColor.rgb ) * getShadowMask();
+
+	// modulation
+	#include <aomap_fragment>
+
+	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + totalEmissiveRadiance;
+
+	#include <envmap_fragment>
+
+	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+
+	#include <tonemapping_fragment>
+	#include <encodings_fragment>
+	#include <fog_fragment>
+	#include <premultiplied_alpha_fragment>
+	#include <dithering_fragment>
+
+}
 #endif //AS_LAMBERT
 #ifdef AS_PHONG
-    #define PHONG
+#define PHONG
 
-    uniform vec3 diffuse;
-    uniform vec3 emissive;
-    uniform vec3 specular;
+uniform vec3 diffuse;
+uniform vec3 emissive;
+uniform vec3 specular;
+uniform float shininess;
+uniform float opacity;
 
-    uniform float shininess;
-    uniform float opacity;
-    uniform float emissiveIntensity;
+#include <common>
+#include <packing>
+#include <dithering_pars_fragment>
+#include <color_pars_fragment>
+#include <uv_pars_fragment>
+#include <uv2_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <aomap_pars_fragment>
+#include <lightmap_pars_fragment>
+#include <emissivemap_pars_fragment>
+#include <envmap_pars_fragment>
+#include <gradientmap_pars_fragment>
+#include <fog_pars_fragment>
+#include <bsdfs>
+#include <lights_pars_begin>
+#include <lights_phong_pars_fragment>
+#include <shadowmap_pars_fragment>
+#include <bumpmap_pars_fragment>
+#include <normalmap_pars_fragment>
+#include <specularmap_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <clipping_planes_pars_fragment>
 
-    #include <common>
-    #include <packing>
-    #include <dithering_pars_fragment>
-    #include <color_pars_fragment>
-    #include <uv_pars_fragment>
-    #include <uv2_pars_fragment>
-    #include <map_pars_fragment>
-    #include <alphamap_pars_fragment>
-    #include <aomap_pars_fragment>
-    #include <lightmap_pars_fragment>
-    #include <emissivemap_pars_fragment>
-    #include <envmap_pars_fragment>
-    #include <gradientmap_pars_fragment>
-    #include <fog_pars_fragment>
-    #include <bsdfs>
-    #include <lights_pars_begin>
-    #include <lights_phong_pars_fragment>
-    #include <shadowmap_pars_fragment>
-    #include <bumpmap_pars_fragment>
-    #include <normalmap_pars_fragment>
-    #include <specularmap_pars_fragment>
-    #include <logdepthbuf_pars_fragment>
-    #include <clipping_planes_pars_fragment>
+void main() {
+
+	#include <clipping_planes_fragment>
+
+	vec4 diffuseColor = vec4( diffuse, opacity );
+	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+	vec3 totalEmissiveRadiance = emissive;
+
+	#include <logdepthbuf_fragment>
+	#include <map_fragment>
+	#include <color_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+	#include <specularmap_fragment>
+	#include <normal_fragment_begin>
+	#include <normal_fragment_maps>
+	#include <emissivemap_fragment>
+
+	// accumulation
+	#include <lights_phong_fragment>
+	#include <lights_fragment_begin>
+	#include <lights_fragment_maps>
+	#include <lights_fragment_end>
+
+	// modulation
+	#include <aomap_fragment>
+
+	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
+
+	#include <envmap_fragment>
+
+	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+
+	#include <tonemapping_fragment>
+	#include <encodings_fragment>
+	#include <fog_fragment>
+	#include <premultiplied_alpha_fragment>
+	#include <dithering_fragment>
+
+}
 #endif //AS_PHONG
 #ifdef AS_PHYSICAL
     #define PHYSICAL
