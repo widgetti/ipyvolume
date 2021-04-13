717efad99061c5d743c93b99d094fd8e4c11ac67
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-04-13 14:27:07 +0200

    🔖 js 0.6.0-alpha.8 released

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -1,7 +1,7 @@
 
 __version_tuple__ = (0, 6, 0, 'alpha.8')
 __version__ = '0.6.0-alpha.8'
-__version_tuple_js__ = (0, 6, 0, 'alpha.7')
-__version_js__ = '0.6.0-alpha.7'
+__version_tuple_js__ = (0, 6, 0, 'alpha.8')
+__version_js__ = '0.6.0-alpha.8'
 # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
 __version_threejs__ = '0.97'
