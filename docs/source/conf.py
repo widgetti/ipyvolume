f3f1066f01fed9ff1b5062aae06842b953d7dd49
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-10-29 16:57:37 +0200

    docs: update with slice and avoid jupyter_sphinx
    Together with nbsphinx, this caused the widgets to render twice.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'jupyter_sphinx.embed_widgets',
     'sphinx_gallery.load_style',
 	'nbsphinx',
 ]
@@ -386,5 +385,6 @@ nbsphinx_thumbnails = {
     'examples/bars': 'examples/screenshot/bars.gif',
     'examples/popup': 'examples/screenshot/ipyvolume-popup-legend-iris.gif',
     'examples/lighting': 'examples/screenshot/volume-rendering-specular.gif',
+    'examples/slice': 'examples/screenshot/ipyvolume-slice-head.gif',
 }
 exclude_patterns = ['**.ipynb_checkpoints']
