51b0cee538632ffa9dd49f6d4cf6f4066af1f777
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-02-24 21:40:46 +0100

    Bump version: 0.6.0 → 0.6.1

--- ipyvolume/_version.py
+++ ipyvolume/_version.py
@@ -1,5 +1,5 @@
 
-__version__ = '0.6.0'
-__version_js__ = '0.6.0'
+__version__ = '0.6.1'
+__version_js__ = '0.6.1'
 # kept for embedding in offline mode, we don't care about the patch version since it should be compatible
 __version_threejs__ = '0.97'
